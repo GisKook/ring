88b3cc49218493aff176e83fc60dc37ee7fd92d9
zhangkai <zhangkai.gis@163.com>
2018-05-07 14:19:49 +0800

    add logrt init

--- socket_server/protocol/protocol_distribute_logrt.go
+++ socket_server/protocol/protocol_distribute_logrt.go
@@ -2,17 +2,30 @@ package protocol
 
 import (
 	"github.com/giskook/ring/pb/common"
+	"strconv"
 )
 
 type DistributeLogRtPkg struct {
-	Imei   string
-	Time   string
-	Result string
+	Imei     string
+	Time     string
+	Result   string
+	Settings map[string]string
 }
 
 func (d *DistributeLogRtPkg) Serialize() []byte {
 	cmd := write_header(PROTOCOL_DISTRIBUTE_LOGRT, d.Imei)
 	cmd += d.Time
+	cmd += PROTOCOL_SEP
+
+	setting_len := len(d.Settings)
+	var cur_inx int
+	for k, v := range d.Settings {
+		cmd += k + "," + v
+		cur_inx, _ = strconv.Atoi(k)
+		if cur_inx < setting_len-1 {
+			cmd += ";"
+		}
+	}
 	cmd += PROTOCOL_SEP
 	cmd += d.Result
 	cmd += PROTOCOL_END_FLAG
@@ -21,9 +34,15 @@ func (d *DistributeLogRtPkg) Serialize() []byte {
 }
 
 func ParseDistributeLogRt(d *Carrier.Distribute) (string, *DistributeLogRtPkg) {
-	return d.Logrt.Imei, &DistributeLogRtPkg{
-		Imei:   d.Logrt.Imei,
-		Time:   d.Logrt.Time,
-		Result: d.Logrt.Result,
+	dlogrt := &DistributeLogRtPkg{
+		Imei:     d.Logrt.Imei,
+		Time:     d.Logrt.Time,
+		Result:   d.Logrt.Result,
+		Settings: make(map[string]string),
 	}
+	for k, v := range d.Logrt.Settings {
+		dlogrt.Settings[k] = v
+	}
+
+	return d.Logrt.Imei, dlogrt
 }
